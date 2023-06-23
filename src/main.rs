32f7b4819549cc727048ae017c2e5d1c5b41b498
Tim Standen <timmstanden@gmail.com>
2023-06-23 12:43:46 -0700

    first commit"

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpStream, TcpListener},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection Established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = 
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+    
+    println!("Request: {:#?}", http_request);
+}
