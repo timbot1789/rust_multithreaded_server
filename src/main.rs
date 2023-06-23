2e57833db98a26cb7ee7b4acf4c3f9c8715fc59f
Tim Standen <timmstanden@gmail.com>
2023-06-23 15:12:17 -0700

    Make the server multithreaded

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,35 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpStream, TcpListener},
+    thread,
+    time::Duration,
 };
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        println!("Connection Established!");
-        handle_connection(stream);
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "html/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "html/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "html/hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "html/404.html"),
     };
-
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
